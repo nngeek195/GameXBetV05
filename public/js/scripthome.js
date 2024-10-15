2632cc6db5f63e66a3998d2f326ce1032c7ece98
Niranga Nayanajith <142929083+nngeek195@users.noreply.github.com>
2024-10-16 03:05:16 +0530

    Add files via upload

---
+++ public/js/scripthome.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+  const savedName = localStorage.getItem('profileName');
+  if (savedName) {
+      document.getElementById('profileName').textContent = savedName;
+  }else {
+    window.location.href= "/index.html";
+  }
+};
+
+// Function to update the user's profile photo on page load
+function updateProfilePhoto() {
+  const profilePhotoElements = document.querySelectorAll('.bg-center.bg-no-repeat.bg-cover');
+  const savedImage = localStorage.getItem('profileImage');
+
+  if (savedImage && profilePhotoElements.length > 0) {
+      profilePhotoElements.forEach(element => {
+          element.style.backgroundImage = `url('${savedImage}')`;
+      });
+  }
+}
+
+
+
+// Call the function when the page loads to ensure the latest photo is shown
+window.onload = function() {
+  updateProfilePhoto();
+};
+
+
+
+
+
+
